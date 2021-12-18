9c92bf8c010a5231834d6375dcb66c5a6a106e21
bzupnick <binny@thirtymadison.com>
2021-12-18 15:31:09 -0500

    added `actualBudgetId` to config and did POC for Actual connection in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const msg = "Saluton!";
+import * as fs from "fs";
 
-console.log(msg);
+let api = require('@actual-app/api');
+let yaml = require('js-yaml');
+
+const config = yaml.load(fs.readFileSync('./configs/investments.yaml', 'utf8'));
+
+api.runWithBudget(config.actualBudgetId, main);
+
+async function main () {
+    const accounts = await api.getAccounts();
+    console.log(accounts);
+}
