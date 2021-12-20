2fd04bcf8433c60899aa7451b1d2249edc289a70
bzupnick <binny@thirtymadison.com>
2021-12-20 14:37:05 -0500

    correctly query the crypto API to get the current worth of crypto

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 import * as fs from "fs";
 
+const axios = require('axios').default;
 let api = require('@actual-app/api');
 let yaml = require('js-yaml');
 
 const config = yaml.load(fs.readFileSync('./configs/investments.yaml', 'utf8'));
+
 api.runWithBudget(config.actualBudgetId, main);
 let accounts: any = null;
 
 async function main () {
     accounts = await api.getAccounts();
-    let curIndexFundBalance: number = null;
-    let curCryptoBalance: number = null;
+    let curIndexFundBalance: number|null = null;
+    let curCryptoBalance: number|null = null;
     if (config.investments.stocks) {
         curIndexFundBalance = await getAccountBalance(config.investments.stocks.actualAccount);
     }
     if (config.investments.crypto) {
         curCryptoBalance = await getAccountBalance(config.investments.crypto.actualAccount);
+    }
+
+    if (curCryptoBalance) {
+        console.log(await getCurrentWorthOfCrypto());
+    }
+
+    if (curIndexFundBalance) {
+        console.log(await getCurrentWorthOfIndexFunds());
     }
 }
 
@@ -28,3 +38,19 @@ let getAccountBalance = async function (name: string) {
     return totalBalance;
 };
 
+
+function getCurrentWorthOfIndexFunds() {
+
+}
+
+async function getCurrentWorthOfCrypto() {
+    let totalWorthOfCrypto = 0;
+    for (const ticker of config.investments.crypto.tickers) {
+        let tickerName = Object.keys(ticker)[0];
+        let tickerAmountOwned = ticker[tickerName];
+        let url = `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=${config.cryptoCompareApiKey}`
+        const cryptoCompareRes = (await axios.get(url)).data.USD
+        totalWorthOfCrypto += cryptoCompareRes * tickerAmountOwned
+    }
+    return totalWorthOfCrypto;
+}
