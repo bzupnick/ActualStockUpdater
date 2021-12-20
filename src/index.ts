eb75849aec0b7b4a115b34c7dfe04bd6bad694b9
bzupnick <binny@thirtymadison.com>
2021-12-19 20:59:46 -0500

    created function to get current account balances and use it twice; once for stocks and once for crypto

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,27 @@ let api = require('@actual-app/api');
 let yaml = require('js-yaml');
 
 const config = yaml.load(fs.readFileSync('./configs/investments.yaml', 'utf8'));
-
 api.runWithBudget(config.actualBudgetId, main);
+let accounts: any = null;
 
 async function main () {
-    const accounts = await api.getAccounts();
-    console.log(accounts);
+    accounts = await api.getAccounts();
+    let curIndexFundBalance: number = null;
+    let curCryptoBalance: number = null;
+    if (config.investments.stocks) {
+        curIndexFundBalance = await getAccountBalance(config.investments.stocks.actualAccount);
+    }
+    if (config.investments.crypto) {
+        curCryptoBalance = await getAccountBalance(config.investments.crypto.actualAccount);
+    }
 }
+
+let getAccountBalance = async function (name: string) {
+    let totalBalance = 0;
+    const accountId = accounts.find((account:any) => account.name === name ).id
+    const allTransactions = await api.getTransactions(accountId, "1980-01-01", "2021-12-20")
+    allTransactions.forEach((k:any) => totalBalance += k.amount)
+    totalBalance = totalBalance / 100;
+    return totalBalance;
+};
+
