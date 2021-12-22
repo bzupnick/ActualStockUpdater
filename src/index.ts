dedc4dcb1442cfae4dd53482cfbaa7d4a5e9c166
bzupnick <binny@thirtymadison.com>
2021-12-21 21:54:06 -0500

    correctly query the index funds API to get the current worth of stocks

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as fs from "fs";
 
 const axios = require('axios').default;
+let Stocks = require('stocks.js')
 let api = require('@actual-app/api');
 let yaml = require('js-yaml');
 
 const config = yaml.load(fs.readFileSync('./configs/investments.yaml', 'utf8'));
+const stocks = new Stocks(config.alphaAdvantageApiKey);
 
 api.runWithBudget(config.actualBudgetId, main);
 let accounts: any = null;
@@ -39,8 +41,26 @@ let getAccountBalance = async function (name: string) {
 };
 
 
-function getCurrentWorthOfIndexFunds() {
+async function getCurrentWorthOfIndexFunds() {
+    let totalWorthOfIndexFunds = 0;
+    for (const ticker of config.investments.stocks.tickers) {
+        let tickerName = Object.keys(ticker)[0];
+        let tickerAmountOwned = ticker[tickerName];
 
+        let result = await stocks.timeSeries({
+            symbol: tickerName,
+            interval: '1min',
+            amount: 1
+        });
+
+        if (result.length == 0) {
+            // do something because the API failed
+            console.log("API FAILED")
+        } else {
+            totalWorthOfIndexFunds += result[0].open * tickerAmountOwned
+        }
+    }
+    return totalWorthOfIndexFunds;
 }
 
 async function getCurrentWorthOfCrypto() {
@@ -48,9 +68,48 @@ async function getCurrentWorthOfCrypto() {
     for (const ticker of config.investments.crypto.tickers) {
         let tickerName = Object.keys(ticker)[0];
         let tickerAmountOwned = ticker[tickerName];
+
         let url = `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=${config.cryptoCompareApiKey}`
-        const cryptoCompareRes = (await axios.get(url)).data.USD
-        totalWorthOfCrypto += cryptoCompareRes * tickerAmountOwned
+        const result = (await axios.get(url)).data.USD
+
+        totalWorthOfCrypto += result * tickerAmountOwned
     }
     return totalWorthOfCrypto;
 }
+//
+// function post_transaction_to_update_account(needed_transaction, account_id, payee_name) {
+//     var endpoint = "/budgets/" + BUDGET_ID + "/transactions";
+//     var payload = {
+//         "transaction": {
+//             "account_id": account_id,
+//             "amount": needed_transaction,
+//             "date": Utilities.formatDate(new Date(), 'America/New_York', 'yyyy-MM-dd'),
+//             "payee_name": payee_name,
+//             "memo": (new Date().getHours() >= 12) ? "Market Close" : "Market Open",
+//             "cleared": "cleared",
+//             "approved": true
+//         }
+//     };
+//     var res = call_ynab_api("post", endpoint, payload)
+//     return res;
+// }
+//
+// function reconcile_index_funds() {
+//     var current_ynab_index_fund = get_current_index_fund_balance();
+//     var updated_index_fund_assets_from_google_sheet = get_updated_index_fund_assets_from_google_sheet()
+//
+//     var index_fund_difference = updated_index_fund_assets_from_google_sheet - current_ynab_index_fund
+//     if(index_fund_difference > 1 || index_fund_difference < -1) {
+//         post_transaction_to_update_account(Math.round(index_fund_difference), INDEX_FUND_ACCOUNT_ID, "Stock Market");
+//     }
+// }
+//
+// function reconcile_crypto() {
+//     var current_ynab_coinbase = get_current_coinbase_balance();
+//     var updated_crypto_assets_from_google_sheet = get_updated_crypto_assets_from_google_sheet()
+//
+//     var crypto_difference = updated_crypto_assets_from_google_sheet - current_ynab_coinbase
+//     if(crypto_difference > 1 || crypto_difference < -1) {
+//         post_transaction_to_update_account(Math.round(crypto_difference), COINBASE_ACCOUNT_ID, "Crypto Gods");
+//     }
+// }
