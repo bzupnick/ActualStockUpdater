32c1dc1e9e5fa24085ab831a8eb3de5ce00bfe3b
bzupnick <binny@thirtymadison.com>
2021-12-23 14:46:47 -0500

    finished it up!

--- src/actual.ts
+++ src/actual.ts
@@ -1,12 +1,44 @@
 export const api = require('@actual-app/api');
+import { config } from './config'
+
+let accounts: any;
+const curDate = getCurDate();
 
 export const getAccountBalance = async function (name: string) {
-    const accounts = await api.getAccounts();
-
+    accounts = await api.getAccounts();
+    let accountId = accountNameToId(name);
     let totalBalance = 0;
-    const accountId = accounts.find((account:any) => account.name === name ).id
-    const allTransactions = await api.getTransactions(accountId, "1980-01-01", "2021-12-20")
+    const allTransactions = await api.getTransactions(accountId, "1980-01-01", curDate)
     allTransactions.forEach((k:any) => totalBalance += k.amount)
     totalBalance = totalBalance / 100;
     return totalBalance;
 };
+
+export const addTransaction = async function (accountName: string, payee: string, amount: number) {
+    let accountId = accountNameToId(accountName);
+    amount = Math.floor(amount*100); // this converts 1.23 to 123 and 1.2345678 to 123
+    await api.addTransactions(accountId,[{
+        account: accountId,
+        date: curDate,
+        amount: amount,
+        payee: await payeeNameToId(payee)
+    }])
+}
+
+function accountNameToId(name: string) {
+    return accounts.find((account:any) => account.name === name ).id
+}
+
+async function payeeNameToId(name: string) {
+    let payees = await api.getPayees();
+    return payees.find((payee:any) => payee.name === name ).id
+}
+
+function getCurDate() {
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+
+    return `${year}-${month}-${date}`;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getAccountBalance, api } from './actual'
+import { getAccountBalance, api, addTransaction } from './actual'
 import { config } from './config'
 import { Stocks } from './stocks'
 import { Crypto } from './crypto'
@@ -13,16 +13,31 @@ api.runWithBudget(config.actualBudgetId, main);
 
 async function main () {
     let currentBalances = new Map();
+    let worthOfInvestments = new Map();
 
+    // get current balances of Actual accounts
     for (const investmentType of investmentTypes) {
         if (config.investments[investmentType]) {
             currentBalances.set(investmentType, await getAccountBalance(config.investments[investmentType].actualAccount))
         }
     }
 
+    // get the amount your investments are actually worth
     for (const investmentType of investmentTypes) {
         if (config.investments[investmentType]) {
-            console.log(await calculateInvestmentTypeWorth.get(investmentType)?.getCurrentWorth());
+            worthOfInvestments.set(investmentType, await calculateInvestmentTypeWorth.get(investmentType)?.getCurrentWorth());
+        }
+    }
+
+    // get the difs of the Actual balance and actual worth of the investments
+    for (const investmentType of investmentTypes) {
+        let dif = worthOfInvestments.get(investmentType) - currentBalances.get(investmentType);
+        if (dif !== 0) {
+            await addTransaction(
+                config.investments[investmentType].actualAccount,
+                config.investments[investmentType].payeeName,
+                dif
+            )
         }
     }
 }
