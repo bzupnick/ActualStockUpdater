d7ee80f2671fc420c3f60dff5f715084f18eb6bc
bzupnick <binny@thirtymadison.com>
2021-12-23 12:10:27 -0500

    uncommented out API calls

--- src/stocks.ts
+++ src/stocks.ts
@@ -6,24 +6,24 @@ const stocksJS = new StocksJS(config.alphaAdvantageApiKey);
 
 export class Stocks implements InvestmentType {
     async getCurrentWorth() {
-        let totalWorthOfIndexFunds = 500;
-        // for (const ticker of config.investments.stocks.tickers) {
-        //     let tickerName = Object.keys(ticker)[0];
-        //     let tickerAmountOwned = ticker[tickerName];
-        //
-        //     let result = await stocks.timeSeries({
-        //         symbol: tickerName,
-        //         interval: '1min',
-        //         amount: 1
-        //     });
-        //
-        //     if (result.length == 0) {
-        //         // do something because the API failed
-        //         console.log("API FAILED")
-        //     } else {
-        //         totalWorthOfIndexFunds += result[0].open * tickerAmountOwned
-        //     }
-        // }
+        let totalWorthOfIndexFunds = 0;
+        for (const ticker of config.investments.stocks.tickers) {
+            let tickerName = Object.keys(ticker)[0];
+            let tickerAmountOwned = ticker[tickerName];
+
+            let result = await stocksJS.timeSeries({
+                symbol: tickerName,
+                interval: '1min',
+                amount: 1
+            });
+
+            if (result.length == 0) {
+                // do something because the API failed
+                console.log("API FAILED")
+            } else {
+                totalWorthOfIndexFunds += result[0].open * tickerAmountOwned
+            }
+        }
         return totalWorthOfIndexFunds;
     }
 }
